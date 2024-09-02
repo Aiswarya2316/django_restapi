c1119a1493970ccfd8fee8a860f19e833fb23a3d
Aiswarya2316 <aiswaryapinkley@gmail.com>
2024-09-02 22:49:19 +0530

    admin

--- project/app/views.py
+++ project/app/views.py
@@ -6,3 +6,7 @@ def dictionary(req):
     d={'name':'abc' , 'age':21 , 'place':'vkm'}
    
     return HttpResponse(d)
+
+def std(req):
+    d={'name':'abc' , 'age':21 , 'place':'vkm'}
+    return HttpResponse(d)
