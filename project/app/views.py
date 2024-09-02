70b431f826a7d759b2bd79a2de2770b9de33ea31
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-09-02 11:31:10 +0530

    restapi

---
+++ project/app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def dictionary(req):
+    d={'name':'abc' , 'age':21 , 'place':'vkm'}
+   
+    return HttpResponse(d)
